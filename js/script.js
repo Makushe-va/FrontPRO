45c5ff22628e7474b2e11817058581b32ec9ebba
Makushe-va <dashamakusheva1705@gmail.com>
2025-05-25 12:28:45 +0300

    User interaction for hw-5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 'use strict';
 
-let name = prompt('What is your name?');
-alert(`Hello, ${name}! How are you?`);
+let name = prompt('Назвіть своє імʼя');
+if (name) {
+    let showGreeting = confirm(`Ви хочете бачити привітання?`);
+    if (showGreeting) {
+        alert(`Привіт, ${name}. Бажаємо Вам гарного дня`)
+    }
+}
