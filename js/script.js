6bc5d57027629c505d91bbf31f97a6279126458e
Makushe-va <dashamakusheva1705@gmail.com>
2025-05-28 20:21:15 +0300

    1.2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 'use strict';
 
-let name = prompt('Назвіть своє імʼя');
-if (name) {
-    let showGreeting = confirm(`Ви хочете бачити привітання?`);
-    if (showGreeting) {
-        alert(`Привіт, ${name}. Бажаємо Вам гарного дня`)
-    }
-}
+let userName = prompt(`Назвіть своє імʼя`);
+console.log(`Імʼя`, userName, typeof userName);
+
+let userAge = Number (prompt(`Скільки Вам років?`));
+console.log(`Вік`, userAge, typeof userAge);
+
+let userConsent = confirm(`Чи згодні Ви прийняти участь?`);
+console.log(`Згода`, userConsent, typeof userConsent);
+
+let userId = 661653686312468386n;
+console.log(`ID` , userId, typeof userId);
+
+let bonusCode = null;
+console.log(`Бонус-код`, bonusCode, typeof bonusCode);
+
+let secondName;
+console.log(`Прізвище`, secondName, typeof secondName);
+
+alert(`Привіт, ${userName}!
+Твій вік ${userAge}
+Твій статус ${userConsent}
+`);
+
+
+
+
+
+
+
+
+
