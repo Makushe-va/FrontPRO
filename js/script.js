4b2637ed1810830eab245022910d6a5bb0b30c8a
Makushe-va <dashamakusheva1705@gmail.com>
2025-05-29 18:49:55 +0300

    ДЗ 6. Cимулятор користувача для вебдодатку

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 'use strict';
+// Імʼя користувача фіксоване тому const
+const userName =`John Doe`;
 
-let userName = prompt(`Назвіть своє імʼя`);
-console.log(`Імʼя`, userName, typeof userName);
+// Вік користувача може змінюватися тому let
+let userAge = 25;
 
-let userAge = Number (prompt(`Скільки Вам років?`));
-console.log(`Вік`, userAge, typeof userAge);
+// Чи верифікований користувач? Статус верифікації незмінний тому const
+const isVerified = true;
 
-let userConsent = confirm(`Чи згодні Ви прийняти участь?`);
-console.log(`Згода`, userConsent, typeof userConsent);
+//Дата реєстрації не зміна тому const
+const registrationDate = "2025-05-10";
 
-let userId = 661653686312468386n;
-console.log(`ID` , userId, typeof userId);
+//Номер картки користувача незмінний тому const
+const cardNumber = 66165368631246833286n;
 
-let bonusCode = null;
-console.log(`Бонус-код`, bonusCode, typeof bonusCode);
+// Промокод якщо є. Може змінитися значення тому let
+let promoCode = null;
 
-let secondName;
-console.log(`Прізвище`, secondName, typeof secondName);
+// Резервне імʼя undefined але може зʼявитися тому let
+let reserveName;
 
-alert(`Привіт, ${userName}!
-Твій вік ${userAge}
-Твій статус ${userConsent}
-`);
+// Кількість бонусів числом, Може змінитися значення тому let
+let bonusBalance = 250;
 
+console.log(`Користувач ${userName}, вік ${userAge}, має бонусів ${bonusBalance}`);
+console.log(`Верифікація: ${isVerified ? "так" : "ні"}. Промокод: ${promoCode ?? "немає"}.`);
+console.log(`Зареєстрований з ${registrationDate}. Номер картки: ${cardNumber}.`);
 
-
-
-
-
-
-
-
+console.log("Ім’я:", typeof userName);
+console.log("Вік:", typeof userAge);
+console.log("Верифікований:", typeof isVerified);
+console.log("Дата реєстрації:", typeof registrationDate);
+console.log("Номер картки:", typeof cardNumber);
+console.log("Промокод:", typeof promoCode);
+console.log("Резервне ім’я:", typeof reserveName);
+console.log("Бонуси:", typeof bonusBalance);
