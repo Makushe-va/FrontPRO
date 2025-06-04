22de94349557fa15a37499256862e6572b102925
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-04 21:39:48 +0300

    ДЗ 13 Години в секунди

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# ДЗ 14. Виконувати другим: Середнє значення
-Create a script that should determine the arithmetic mean of three numbers with the following conditions:
-1) get three numbers from the user after three prompts
-2) show through alert the arithmetic average of these numbers
+# ДЗ 13. Години в секунди
+Write a script that converts hours to seconds and should do the following:
+1) ask the user for the number of hours;
+2) count how many seconds there are in this number of hours;
+3) write the calculated value into a variable;
+4) output this variable to the user via alert.
--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 'use strict';
 
-// We ask the user for three numbers:
+// We ask the user for the number of hours:
 
-let num1 = Number ( prompt ('Enter the first number'));
-let num2 = Number ( prompt ('Enter the second number'));
-let num3 = Number (prompt('Enter the third number'));
+let hours = prompt('Enter the number of hours');
 
-// We calculate the arithmetic mean:
-let averageValue = (num1 + num2 + num3)/3;
-console.log(averageValue);
+// We convert the entered value into a number:
+
+hours = Number(hours);
+
+//Calculate the number of second - 1 hours = 3600 seconds:
+let seconds = hours * 3600;
+
+// Infers the result:
+alert(`At ${hours} o'clock there are ${seconds} seconds `);
