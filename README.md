fb19817073cd91a22bc3c3d6d25543540dabda32
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-04 21:26:58 +0300

    ДЗ 14 Середнє значення

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# ДЗ 11. Особиста інформація + сесія в браузері
-You need to create a program that:
-1. Receives input from the user via prompt and confirm
-2. Creates a session object containing all the collected information
-3. Has a nested object and an array
-4. Uses all data types
-
-Steps:
-1. Data collection via browser:
-a) Ask for a name (prompt) → string;
-b) Ask for age → number (convert from a string);
-c) Ask if the user wants to receive notifications (confirm) → boolean;
-d) Create a fake userId as a bigInt (you can just manually write 1234567890123456789n);
-e) Set the lastLogin field to null;
-f) Create a nickname field, but do not set a value (leave undefined);
-g) Add a favoriteTech array of three technologies (any);
-h)Add a nested settings object with fields:
-    -theme: "dark" or "light"
-    -autoLogin: false
-2. Create a session object that will contain all this data.
-3. Output:
-   1) alert with short information: "Hello, [name]! Your ID is [userId]"
-   2) In console.log — the entire object
-   3) In console.log — the type of each field (via typeof)
+# ДЗ 14. Виконувати другим: Середнє значення
+Create a script that should determine the arithmetic mean of three numbers with the following conditions:
+1) get three numbers from the user after three prompts
+2) show through alert the arithmetic average of these numbers
--- js/script.js
+++ js/script.js
@@ -1,45 +1,11 @@
 'use strict';
 
-// Data collection via browser:
+// We ask the user for three numbers:
 
-const name = prompt('What is your name?');
-let age = Number(prompt('What is your age?'));
-const wantsNotifications = confirm('Do you want to receive notifications?');
-const userID = 12345678900998765556n;
-const lastLogin = null;
-let nickName;
-const favoriteTech = ["JavaScript", "HTML", "CSS"];
-const settings = {
-    theme: 'dark',
-    autologin: false,
-}
+let num1 = Number ( prompt ('Enter the first number'));
+let num2 = Number ( prompt ('Enter the second number'));
+let num3 = Number (prompt('Enter the third number'));
 
-// Creates a session object containing all the collected information(user name, user age.....)
-
-const session = {
-    name,
-    age,
-    wantsNotifications,
-    userID,
-    lastLogin,
-    nickName,
-    favoriteTech,
-    settings
-};
-
-// display alert.
-alert(`Привіт, ${session.name}! Ваш ID: ${session.userID}`);
-
-// We output the entire object to the console.
-console.log(session);
-
-//We output the type of each field to the console.
-console.log('name', typeof name);
-console.log('age', typeof age);
-console.log('wantsNotifications', typeof wantsNotifications);
-console.log('userID', typeof userID);
-console.log('lastLogin', typeof lastLogin);
-console.log('nickName', typeof nickName);
-console.log('favoriteTech', typeof favoriteTech);
-console.log('settings', typeof settings);
-
+// We calculate the arithmetic mean:
+let averageValue = (num1 + num2 + num3)/3;
+console.log(averageValue);
