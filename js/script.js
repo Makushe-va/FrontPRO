7c74cbfca6525d996c97cd1f3b186fc22848ef52
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-05 19:27:30 +0300

    ДЗ 14 Середнє значення 2.0

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 // We ask the user for three numbers:
 
-let num1 = Number ( prompt ('Enter the first number'));
-let num2 = Number ( prompt ('Enter the second number'));
-let num3 = Number (prompt('Enter the third number'));
+const num1 = Number ( prompt ('Enter the first number'));
+const num2 = Number ( prompt ('Enter the second number'));
+const num3 = Number (prompt('Enter the third number'));
 
 // We calculate the arithmetic mean:
-let averageValue = (num1 + num2 + num3)/3;
+const averageValue = (num1 + num2 + num3)/3;
 console.log(averageValue);
