458989224670b868d55ad2f4db512ba22aecbaf6
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-05 19:22:31 +0300

    ДЗ 13 Години в секунди 2.0

--- js/script.js
+++ js/script.js
@@ -2,14 +2,10 @@
 
 // We ask the user for the number of hours:
 
-let hours = prompt('Enter the number of hours');
-
-// We convert the entered value into a number:
-
-hours = Number(hours);
+const hours = +prompt('Enter the number of hours');
 
 //Calculate the number of second - 1 hours = 3600 seconds:
-let seconds = hours * 3600;
+const seconds = hours * 3600;
 
 // Infers the result:
 alert(`At ${hours} o'clock there are ${seconds} seconds `);
