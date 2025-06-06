a5f0624534d915cc1c12c365f023581288f964e3
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-06 09:55:29 +0300

    ДЗ 15. Портрет користувача

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# ДЗ 14. Виконувати другим: Середнє значення
-Create a script that should determine the arithmetic mean of three numbers with the following conditions:
-1) get three numbers from the user after three prompts
-2) show through alert the arithmetic average of these numbers
+# ДЗ 15.  Виконувати третім: Портрет користувача
+
+Part 1
+Create a script that should do the following:
+1) ask the user for their year of birth;
+2) ask him in which city he lives;
+3) ask his favorite sport.
+   When you click on OK, we show a window where the following information should be displayed:
+1) his age;
+2) if the user indicates Kyiv, Washington or London, then show him the message - "You live in the capital..." and instead of dots we substitute the country whose capital is the city. Otherwise, we show him "you live in the city...", where the location of the points is the entered city.
+
+Part 2
+We choose three sports and three champions in these sports. Accordingly, if the user specifies one of these sports, we show him the message "Cool! Do you want to become ...?" and replace the dots with the name and surname of the champion.
+
+All this should be displayed in one window (alert).
+If in some case he does not want to enter information and clicks Cancel, show him the message - "It's a pity that you did not want to enter your ..." and indicate that he did not want to enter - date of birth, city or sport.
--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
 'use strict';
 
-// We ask the user for three numbers:
+// We ask the user his year of birth, in which city he lives and what is his favorite sport.
+// If he click "CANCEL" we display a message in Alert
 
-const num1 = Number ( prompt ('Enter the first number'));
-const num2 = Number ( prompt ('Enter the second number'));
-const num3 = Number (prompt('Enter the third number'));
+const yearBirth = prompt('Enter your year of birth');
+if (yearBirth === null) {
+    alert("It's a pity that you didn't enter the year of birth.");
+} else {
+   // We calculate the age of the user, for this we need the current date and the year of birth entered by the user
+    const year = +yearBirth;
+    const currentYear = new Date().getFullYear();
+    const ageUser = currentYear - year;
 
-// We calculate the arithmetic mean:
-const averageValue = (num1 + num2 + num3)/3;
-console.log(averageValue);
+    const city = prompt('In which city do you live?');
+    if (city === null) {
+        alert("It's a pity that you didn't want to write your city.");
+    } else {
+        const sport = prompt('What is your favorite sport?');
+        if (sport === null) {
+            alert("It's a pity that you didn't want to write about your favorite sport.");
+        } else {
+//We create a message about the city:
+let cityMessage;
+switch (city){
+    case 'Kyiv':
+        cityMessage = `You live in the capital of Ukraine!`;
+        break;
+        case 'London':
+            cityMessage = `You live in the capital of Great Britain!`;
+            break;
+            case 'Washington':
+                cityMessage = `You live in the capital of USA!`;
+                break;
+                default:
+                    cityMessage = `You live in the city ${city}!`;
+}
+//We create a message about the sport:
+let sportMessage;
+switch (sport){
+    case 'football':
+        sportMessage = `Cool! Do you want to become like Diego Maradona?`;
+        break;
+        case 'tennis':
+            sportMessage = `Cool! Do you want to become like Rafail Nadal?`;
+            break;
+            case 'hockey':
+                sportMessage = `Cool! Do you want to become like Wayne Gretzky?`;
+                break;
+                default:
+                    sportMessage = `Your favorite sport ${sport}`
+}
+// We show everything in one alert:
+
+alert(`You are ${ageUser} years old ${cityMessage}, ${sportMessage}`);}}}
