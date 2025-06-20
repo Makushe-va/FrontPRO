09abebbb6bc8fb194c04efb63f26517d5770c38e
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-20 16:19:42 +0300

    ДЗ 23. Реалізуйте функцію removeElement

--- js/script.js
+++ js/script.js
@@ -1,40 +1,12 @@
 'use strict';
 
-const numPositive = (arr) => {
-    const positiveArr = [];
-    if (arr.length === 0) return 'The array is empty';
-    for (let i = 0; i < arr.length; i++) {
-        const current = arr[i];
-        if (current > 0) {
-            positiveArr.push(current);
-        }
+const removeArray = (array, item) =>{
+    const index = array.indexOf(item);
+    if (index !== 1){
+        array.splice(index, 1);
     }
-    return positiveArr.length > 0 ? positiveArr : null;
-}
-const arr = [1,2,3,4,5,-1,-2,-3,-4,-5];
-console.log(numPositive(arr));
+};
 
-
-
-
-//
-// function numPositiveOne(arr){
-//     const positiveArr = [];
-//     if(arr.length === 0){
-//         return 'The array is empty';
-//     }
-//     for(let i = 0; i < arr.length; i++){
-//         const current = arr[i];
-//         if(current > 0){
-//             positiveArr.push(current);
-//         }
-//     }
-//     if(positiveArr.length > 0){
-//         return positiveArr;
-//     } else {
-//         return null;
-//     }
-//
-// }
-// const arr1 = [1,2,3,4,5,-1,-2,-3,-4,-5];
-// console.log(numPositiveOne(arr1));
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+removeArray(array, 5);
+console.log(array);
