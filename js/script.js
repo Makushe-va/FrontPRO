cd8fb37222cba8e6a7e28a41356c4fb6d24227e2
Makushe-va <dashamakusheva1705@gmail.com>
2025-06-29 15:41:54 +0300

    ДЗ 28. Хойстинг (Hoisting) та області видимості (Scope)

--- js/script.js
+++ js/script.js
@@ -1,75 +1,22 @@
 'use strict';
-let users = [
-    {
-        "index": 0,
-        "isActive": true,
-        "balance": "$2,226.60",
-        "name": "Eugenia Sawyer",
-        "gender": "female",
-        "phone": "+1 (840) 583-3207",
-        "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-        "index": 1,
-        "isActive": true,
-        "balance": "$2,613.77",
-        "name": "Pauline Gallegos",
-        "gender": "female",
-        "phone": "+1 (985) 593-3328",
-        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-        "index": 2,
-        "isActive": false,
-        "balance": "$3,976.41",
-        "name": "Middleton Chaney",
-        "gender": "male",
-        "phone": "+1 (995) 591-2478",
-        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-        "index": 3,
-        "isActive": true,
-        "balance": "$1,934.58",
-        "name": "Burns Poole",
-        "gender": "male",
-        "phone": "+1 (885) 559-3422",
-        "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-        "index": 4,
-        "isActive": true,
-        "balance": "$3,261.65",
-        "name": "Mcfadden Horne",
-        "gender": "male",
-        "phone": "+1 (942) 565-3988",
-        "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-        "index": 5,
-        "isActive": false,
-        "balance": "$1,790.56",
-        "name": "Suzette Lewis",
-        "gender": "female",
-        "phone": "+1 (837) 586-3283",
-        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+
+function hoistingScope() {
+
+    for (let i = 0; i < 1; i++) {
+        var variableVar = "var в циклі";
+        let variableLet = "let в циклі";
+        const variableConst = "const в циклі";
+
+        console.log("variableVar:", variableVar);
+        console.log("variableLet:", variableLet);
+        console.log("variableConst:", variableConst);
     }
-]
-// Converting the balance from a string to a floating point number
-function parseBalance(balanceStr) {
-    return parseFloat(balanceStr.replace(/[$,]/g, ""));
+
+    // var visible outside the loop
+    console.log("variableVar:", variableVar);
+
+    // let and const not visible outside the loop - we will get errors
+    console.log("variableLet:", variableLet);
+    console.log("variableConst:", variableConst);
 }
-// We output an array of phone numbers of users whose balance exceeds 2000 dollars
-//using the filter we select users with balances > 2000 and using map we extract an array of phones
-let phonesOver2000 = users
-  .filter(user => parseBalance(user.balance) > 2000)
-  .map(user => user.phone);
-
-console.log("Телефони користувачів із балансом понад $2000:");
-console.log(phonesOver2000);
-
-//Find the sum of all user balances
-let sumBalance = users
-    .reduce((sum, user) => sum + parseBalance(user.balance), 0);
-
-console.log(sumBalance);
+hoistingScope();
